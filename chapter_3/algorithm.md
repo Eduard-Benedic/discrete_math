4c9f899cfc597f9b3eb2b101f0efc1bc92f857fd
Eduard Benedic <benediceduard@outlook.com>
2021-02-23 22:22:44 +0000

    binary search

--- chapter_3/algorithm.md
+++ chapter_3/algorithm.md
@@ -2,3 +2,20 @@
 
 ## Def 1
 An algorithm is a finite sequence of precis instructions for performing a computation or for solving a problem.
+
+An algorithm should have the following characteristics:
+1. Input
+2. Ouput
+3. Definiteness
+4. Correctness
+5. Finiteness
+6. Effectiveness
+7. Generality
+
+## Searching algorithms
+
+### The Linear Search (Sequential search)
+The linear seach as the name suggests perform a search from one term to another in a sequencial order.
+
+### The Binary Search
+The binary search is used when the list has terms occurring in order of increasing size.
---
+++ chapter_3/searches.py
@@ -0,0 +1,33 @@
+
+
+# num_sequence = [1,4,5,6,12,20,200]
+
+# def linear_search(arr, num):
+#     arr_length = len(arr)
+#     location = 0
+#     for x in range(len(arr)):
+#         if arr[x] == num:
+#             location = x
+#     return location
+        
+
+# loc_num = linear_search(num_sequence, 20)
+
+
+binary_arr_search = [1, 2, 3, 5, 6, 7, 8, 10, 12, 13, 20, 22]
+
+
+def binary_search(arr, num):
+    half = len(arr) // 2
+    if arr[half] == num:
+        return True
+    if  num < arr[half]:
+        return binary_search(arr[0:half], num)
+    else:
+        return binary_search(arr[half:], num)
+
+founded = binary_search(binary_arr_search, 2)
+
+print(founded)
+
+
