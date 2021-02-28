c68429f5856487a3c6d0f4d0a42fe307ed62b41c
Eduard <benediceduard@outlook.com>
2021-02-28 19:55:06 +0000

    bubble sort implementatino

--- chapter_3/sorting.py
+++ chapter_3/sorting.py
@@ -4,7 +4,6 @@ start = time()
 
 items = [6, 3, 1, 2, 44442, 323,55, 331, 5, 32, 11, 33, 3444, 12393, 3945, 123]
 
-
 def bubble_sort(arraySource):
     for i in range(len(arraySource)):
         shortened_list = len(arraySource) - 1
@@ -13,13 +12,13 @@ def bubble_sort(arraySource):
                 a = arraySource[j + 1]
                 arraySource[j + 1] = arraySource[j]
                 arraySource[j] = a
-        shortened_list -= shortened_list
+        shortened_list -= 1
     return arraySource
 
 sorted_items = bubble_sort(items)
 
 end = time()
 
-print(end - start)
+
 print(sorted_items)
 
